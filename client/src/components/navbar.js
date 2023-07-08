06b318a102eb9b96226b48ddbb46685a3200273c
Michael Brady <michaelbrady712@gmail.com>
2023-07-08 14:48:15 -0500

    Added styles and html to create a responsive navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -6,8 +6,24 @@ export const Navbar = () => {
 
     return (
         <div className="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            <nav class="nav-section">
+                <div id="mobnav">
+                    <input class="checkbox" type="checkbox"/>
+                    <div class="hamburger-lines">
+                        <span class="line line1"></span>
+                        <span class="line line2"></span>
+                        <span class="line line3"></span>
+                    </div>  
+                    <div class="menu-items">
+                        <Link to="/">Home</Link>
+                        <Link to="/login">Login</Link>
+                    </div>
+                </div>
+                <div id="normnav">
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                </div>
+            </nav>
         </div>
     );
 }
