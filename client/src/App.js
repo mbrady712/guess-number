586a24296a30279a5adeaf9cf259a01131af33ac
Michael Brady <michaelbrady712@gmail.com>
2023-07-08 13:48:36 -0500

    Added basic funcionality to switch between home and login pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
