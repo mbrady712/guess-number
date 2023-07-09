95dba17165ac189677bfae3acd26fac717be0ec4
Michael Brady <michaelbrady712@gmail.com>
2023-07-08 19:52:49 -0500

    Added structure to login page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,5 +1,26 @@
 export const Login = () => {
     return (
-        <div>Login</div>
+        <div className="auth">
+            <form id="login">
+                <h2>Login</h2>
+                <div>Email:</div>
+                <input type="text"/>
+                <br/>
+                <div>Password:</div>
+                <input type="text"/>
+                <br/>
+                <button type="submit">Login</button>
+            </form>
+            <form id="register">
+                <h2>Register</h2>
+                <div>Email:</div>
+                <input type="text"/>
+                <br/>
+                <div>Password:</div>
+                <input type="text"/>
+                <br/>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 };
