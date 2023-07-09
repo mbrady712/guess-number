a1c1293b6eb2dbd05d54954169bc5b7ae72bba7f
Michael Brady <michaelbrady712@gmail.com>
2023-07-09 11:54:48 -0500

    Connected server to MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
@@ -8,3 +9,9 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(3001, () => console.log("SERVER STARTED"));
+
+dotenv.config();
+
+const connectionString = process.env.ATLAS_URI;
+
+mongoose.connect(connectionString);
