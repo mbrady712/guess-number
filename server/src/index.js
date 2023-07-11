4755d722fc511b83fc336fe810f82266ed5de2c1
Michael Brady <michaelbrady712@gmail.com>
2023-07-10 22:53:37 -0500

    Installed axios

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,16 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import { userRouter } from './routes/users.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(3001, () => console.log("SERVER STARTED"));
+
+app.use("/auth", userRouter);
 
 dotenv.config();
 
