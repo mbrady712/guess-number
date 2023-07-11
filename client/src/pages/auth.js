977a7e69f6c2f774edd455bebc2cb33e9ada700d
Michael Brady <michaelbrady712@gmail.com>
2023-07-11 13:00:00 -0500

    Modified login mechanism

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useCookies } from 'react-cookie';
+import { useNavigate } from "react-router-dom";
+
 export const Auth = () => {
     return (
         <div className="auth">
@@ -10,14 +13,63 @@ export const Auth = () => {
 };
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [ , setCookies] = useCookies(["access_token"]);
+
+    const navigate = useNavigate();
+
+    const loginSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:3001/auth/login", {
+                username,
+                password
+            });
+            
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID);
+            navigate("/");
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return(
-        <form id="login">
+        <LoginForm   
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            onSubmit={loginSubmit}
+        />
+    );
+}
+
+const LoginForm = ({
+    username, 
+    setUsername, 
+    password, 
+    setPassword, 
+    onSubmit
+}) => {
+    return (
+        <form id="login" onSubmit={onSubmit}>
             <h2>Login</h2>
+            <br/>
             <div>Username:</div>
-            <input type="text"/>
+            <input 
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            />
             <br/>
             <div>Password:</div>
-            <input type="text"/>
+            <input type="text"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            />
             <br/>
             <button type="submit">Login</button>
         </form>
